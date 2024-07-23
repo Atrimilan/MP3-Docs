766018321359289130fd49b5e431fcce01d537f5
Milan NICOLAS <46848005+Atrimilan@users.noreply.github.com>
2024-07-23 22:52:21 +0200

    Add table with world coordinates

---
+++ minecraft/serveur_survie/coordonnees/ts/coordinate-table.tsx
@@ -0,0 +1,56 @@
+
+import ContentCopyRoundedIcon from '@mui/icons-material/ContentCopyRounded'; // https://mui.com/material-ui/material-icons/
+import { useState } from 'react';
+
+export const CoordinatesTable = ({ data }) => {
+
+    const [isPressed, setIsPressed] = useState({});
+
+    const handleMouseDown = (index) => {
+        setIsPressed({ ...isPressed, [index]: true });
+    };
+
+    const handleMouseUp = (index) => {
+        setIsPressed({ ...isPressed, [index]: false });
+    };
+
+    const handleMouseLeave = (index) => {
+        setIsPressed({ ...isPressed, [index]: false });
+    };
+
+    return (
+        <table className='coordinates-table'>
+            <thead>
+                <tr>
+                    <th className='coordinates-table-description'>Description</th>
+                    <th className='coordinates-table-coordinates'>Coordonnées</th>
+                    <th>Copier</th>
+                </tr>
+            </thead>
+            <tbody>
+                {data.map((row, index) => (
+                    <tr key={index}>
+                        <td>{row[0]}</td>
+                        <td className='center-text'>{row[1]}</td>
+                        <td className='center-icon'>
+                            <ContentCopyRoundedIcon
+                                className='copy-icon'
+                                onMouseDown={() => handleMouseDown(index)}
+                                onMouseUp={() => handleMouseUp(index)}
+                                onMouseLeave={() => handleMouseLeave(index)}
+                                style={{
+                                    cursor: 'pointer',
+                                    opacity: isPressed[index] ? 0.5 : 1,
+                                }}
+                                onClick={() => {
+                                    navigator.clipboard.writeText(row[1]);
+                                }}
+
+                            />
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    );
+};

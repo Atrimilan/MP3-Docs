07dd1f73e4ec2673efeb86343d689c83354cab80
Milan NICOLAS <46848005+Atrimilan@users.noreply.github.com>
2025-05-17 15:25:45 +0200

    Add creative warp table with new MP3 proxy request

---
+++ components/pl3xmap-warps-table.tsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+import { CoordinatesTable } from './coordinates-table';
+import { Blocks } from 'react-loader-spinner';
+
+const fetchStatus = (server, world, file) => {
+    return fetch(`http://mp3.pixelfucker.com:3000/pl3xmap/${server}/${world}/${file}`, { method: "GET", redirect: "follow" })
+        .then((response) => response.json())
+        .catch((error) => console.error(error));
+};
+
+export const Pl3xmapWarps = ({ data }) => {
+    const [warps, setWarps] = useState(null);
+    const { server, world, file } = data;
+
+    useEffect(() => {
+        fetchStatus(server, world, file).then(data => setWarps(data));
+
+        const intervalId = setInterval(() => {
+            fetchStatus(server, world, file).then(data => {
+                setWarps(data)
+            });
+        }, 10000);
+        return () => clearInterval(intervalId);
+    }, []);
+
+    // Loading message
+    if (!warps) {
+        return (
+            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                <Blocks height="35" width="35" visible={true} /> Chargement...
+            </div>
+        );
+    }
+
+    // Display warps in a table
+    return (
+        <CoordinatesTable data={
+            warps.map(warp => {
+                const { x, z } = warp.data.point;
+                return [warp.options.tooltip.content, `${x} ~ ${z}`]; // "~" is the current player "y"
+            })
+        } />
+    );
+}

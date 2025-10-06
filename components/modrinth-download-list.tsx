0ec53d8876445649da06e35d1414ccc72a168620
Milan NICOLAS <46848005+Atrimilan@users.noreply.github.com>
2025-10-06 21:05:52 +0200

    Use dropdown to select versions from fetched Fabric versions

--- components/modrinth-download-list.tsx
+++ components/modrinth-download-list.tsx
@@ -2,13 +2,27 @@ import { useEffect, useState } from "react";
 import { useHistory, useLocation } from "@docusaurus/router";
 import DownloadButton from "./download-button";
 
+const fetchFabricVersions = async () => {
+    try {
+        // Based on Fabric API versions
+        const response = await fetch(`https://api.modrinth.com/v2/project/P7dR8mSH/version?version_type=release`, { method: "GET", redirect: "follow" });
+        const result = (await response.json());
+        const regex = /^\d+\.\d+(\.\d+)?$/;
+        const gameVersions = result.map((v) => v.game_versions[0]).filter((v) => v && regex.test(v));
+        return [...new Set(gameVersions)].sort((a: string, b: string) => b.localeCompare(a, undefined, { numeric: true }));
+    } catch (error) {
+        console.error("Could not fetch Fabric versions from Modrinth API: ", error);
+        return null;
+    }
+};
+
 const fetchModrinthApi = async (id: string, queryParams: string) => {
     try {
         const response = await fetch(`https://api.modrinth.com/v2/project/${id}/version${queryParams}`, { method: "GET", redirect: "follow" });
         const result = await response.json();
         return result;
     } catch (error) {
-        console.error("Could not fetch modrinth API for project " + id + ": ", error);
+        console.error("Could not fetch Modrinth API for project " + id + ": ", error);
         return null;
     }
 };
@@ -19,20 +33,26 @@ export default function ModrinthDownloadList({ data }) {
     const location = useLocation();
     const history = useHistory();
 
-    const [versionInputValue, setVersionInputValue] = useState("");
+    const [fabricVersions, setFabricVersions] = useState([]);
     const [selectedVersion, setSelectedVersion] = useState("");
     const [projectsData, setProjectsData] = useState({});
     const [unavailableProjects, setUnavailableProjects] = useState(0);
 
-    const handleVersionSubmit = (e) => {
+    /**
+     * Change the selected version and update the "version" query param
+     */
+    const handleVersionChange = (e) => {
         e?.preventDefault();
-        setSelectedVersion(versionInputValue);
+        setSelectedVersion(e.target.value);
 
         const params = new URLSearchParams(location.search);
-        params.set("version", versionInputValue);
+        params.set("version", e.target.value);
         history.replace({ search: params.toString() });
     };
 
+    /**
+     * On page loaded, get the "version" query param and fetch fabric versions
+     */
     useEffect(() => {
         const params = new URLSearchParams(location.search);
         let versionParam = params.get("version");
@@ -44,20 +64,32 @@ export default function ModrinthDownloadList({ data }) {
         }
 
         setSelectedVersion(versionParam);
-        setVersionInputValue(versionParam);
+
+        (async () => {
+            const versions = await fetchFabricVersions();
+            if (versions) {
+                setFabricVersions(versions);
+            }
+        })();
     }, []);
 
     const loader = !!defaultLoader ? encodeURI(`["${defaultLoader}"]`) : "";
     const version = !!selectedVersion ? encodeURI(`["${selectedVersion}"]`) : "";
-    const versionType = "release";
 
+    /**
+     * Fetch Modrinth API for each project
+     */
     useEffect(() => {
+        let cancelled = false;
         setProjectsData({});
         setUnavailableProjects(0);
 
         if (projects.length > 0 && selectedVersion) {
             projects.forEach(async (project) => {
-                const result = await fetchModrinthApi(project.id, `?loaders=${loader}&game_versions=${version}&version_type=${versionType}`);
+                const result = await fetchModrinthApi(project.id, `?loaders=${loader}&game_versions=${version}`); // Could also add &version_type=release
+                
+                if (cancelled) return;
+                
                 if (result && result.length > 0 && result[0].files?.length > 0) {
                     setProjectsData((prev) => ({
                         ...prev,
@@ -67,55 +99,51 @@ export default function ModrinthDownloadList({ data }) {
                         },
                     }));
                 } else {
-                    setUnavailableProjects((unavailableProjects) => ++unavailableProjects);
+                    setUnavailableProjects((prev) => prev + 1);
                 }
             });
         }
+        return () => { cancelled = true }; // Cancel fetch if selecteVersion has changed
     }, [projects, selectedVersion]);
 
     const availableProjects = projects.filter((p) => projectsData[p.id]);
 
     return (
         <>
-            <form onSubmit={handleVersionSubmit}>
-                <input
-                    type="text"
-                    id="version"
-                    name="version"
-                    placeholder="Saisissez une version de Minecraft"
-                    value={versionInputValue}
-                    onChange={(e) => setVersionInputValue(e.target.value)}
-                    pattern="^\d+\.\d+(\.\d+)?$"
-                    required
-                    title="Saisissez une version de Minecraft valide, par exemple : 1.21.8"
+            <div style={{
+                display: "flex",
+                alignItems: "center",
+                borderRadius: "8px",
+                marginBottom: "1rem",
+                border: "1px solid var(--ifm-toc-border-color)",
+                width: "min-content",
+            }}>
+                <p style={{
+                    marginBottom: "0",
+                    whiteSpace: "nowrap",
+                    padding: "0.4rem 1rem",
+                    borderRadius: "8px 0 0 8px",
+                    backgroundColor: "var(--ifm-background-color)"
+                }}>Sélectionnez une version Minecraft :</p>
+                <select
+                    value={selectedVersion}
+                    onChange={handleVersionChange}
                     style={{
-                        width: "30%",
-                        marginBottom: "1rem",
-                        marginRight: "1rem",
-                        padding: "0.7rem 1rem",
-                        backgroundColor: "var(--ifm-pre-background)",
-                        border: "1px solid var(--ifm-toc-border-color)",
+                        width: "150px",
+                        padding: "0.5rem",
                         fontFamily: "var(--ifm-font-family-base)",
                         fontSize: "var(--ifm-font-size-base)",
-                        borderRadius: "4px",
-                        boxSizing: "border-box",
-                    }}
-                />
-                <button
-                    type="submit"
-                    style={{
-                        marginBottom: "1rem",
-                        padding: "0.7rem 1rem",
-                        backgroundColor: "var(--ifm-background-color)",
-                        border: "1px solid var(--ifm-toc-border-color)",
-                        fontFamily: "var(--ifm-font-family-base)",
-                        fontSize: "var(--ifm-font-size-base)",
-                        borderRadius: "4px",
-                        boxSizing: "border-box",
-                        cursor: "pointer",
-                    }}
-                >Rechercher</button>
-            </form>
+                        borderRadius: "0 8px 8px 0",
+                        border: "none",
+                        backgroundColor: "var(--ifm-pre-background)"
+                    }}>
+                    {fabricVersions.map((version) => (
+                        <option key={version} value={version} style={{ backgroundColor: "var(--ifm-background-color)" }}>
+                            {version}
+                        </option>
+                    ))}
+                </select>
+            </div>
             <ul>
                 {
                     !!selectedVersion ? (
